edc0efccec4e04b5b62cd99265dd4ef0a55ea6dc
Aditya Prakash <110540811+gaurav-aditya@users.noreply.github.com>
2023-02-02 15:01:39 +0530

    Add files via upload

---
+++ spring-jpa-1/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Student {
+	@Id
+	Integer sid;
+	String sname;
+	Integer score;
+	public Integer getSid() {
+		return sid;
+	}
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public Integer getScore() {
+		return score;
+	}
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+	
+
+}
