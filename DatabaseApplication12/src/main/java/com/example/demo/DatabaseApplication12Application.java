1528439ad70943b67402f2027e54dc669f21b3b9
Aditya Prakash <110540811+gaurav-aditya@users.noreply.github.com>
2023-02-03 13:58:16 +0530

    Add files via upload
    Date (3 feb 2023)

---
+++ DatabaseApplication12/src/main/java/com/example/demo/DatabaseApplication12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseApplication12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseApplication12Application.class, args);
+	}
+
+}
